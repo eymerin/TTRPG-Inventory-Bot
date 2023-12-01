86277925be725013afc7c6aba2af4a630b902a2a
A04pr <areed041704@gmail.com>
2023-11-30 20:54:18 -0700

    seeds files

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ const routes = require('');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
