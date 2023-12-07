387e237738a2319bd3c3d7feb18bc598a32db2a0
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-12-07 00:09:18 -0700

    send item command pulls items from DB and console logs db options selected.

--- bot/commands/utility/senditem.js
+++ bot/commands/utility/senditem.js
@@ -2,70 +2,97 @@ const { StringSelectMenuBuilder, StringSelectMenuOptionBuilder, ActionRowBuilder
 const { Item, Player } = require('../../../models/index');
 
 module.exports = {
-  // Define the command data using SlashCommandBuilder
-    data: new SlashCommandBuilder()
-      .setName('senditem')
-      .setDescription('Select an Item to send'),
+  data: new SlashCommandBuilder()
+    .setName('senditem')
+    .setDescription('Send item to player!'),
+  async execute(interaction) {
+    try {
+      // Fetch items from the database
+      const items = await Item.findAll();
+      const itemOptions = items.map((item) =>
+        new StringSelectMenuOptionBuilder()
+          .setLabel(item.item_name)
+          .setValue(item.item_id.toString())
+      );
 
-    async execute(interaction) {
-            // Initialize session data for the interaction
-            interaction.sessionData = {
-            selectedItem: null
-            };
-            
-            const items = await Item.findAll();
-            const players = await Player.findAll();
-      
-            // Create a StringSelectMenuBuilder for items
-            const itemSelectMenu = new StringSelectMenuBuilder()
-                .setCustomId('itemSelection')
-                .setPlaceholder('Select an item to send')
-                .setMaxValues(1);
-                
-      
-            // Generate select menu options for items based on item data
-            items.forEach((item) => {
-                itemSelectMenu.addOptions(
-                    new StringSelectMenuOptionBuilder()
-                        .setLabel(item.item_name) // Set label as item name or a relevant field
-                        .setDescription(item.item_description) // Set description as item description or a relevant field
-                        .setValue(item.item_id.toString()) // Set value as a unique identifier for the item
-                        );
-                    });
-      
-            // Create an ActionRow and add the item select menu
-            const itemActionRow = new ActionRowBuilder().addComponents(itemSelectMenu);
-      
-            const playerSelectMenu = new StringSelectMenuBuilder()
-                .setCustomId('playerSelection')
-                .setPlaceholder('Choose a player to send to');
-      
-            players.forEach((player) => {
-                playerSelectMenu.addOptions(
-                    new StringSelectMenuOptionBuilder()
-                        .setLabel(player.name)
-                        .setValue(player.name.toString())
-                        );
-                    });
-      
-            const playerActionRow = new ActionRowBuilder().addComponents(playerSelectMenu);
-      
-            // Send the item select menu in an interaction reply
-            const reply = await interaction.reply(
-                {
-                content: 'Please select an item to send, and the player to send it to:',
-                components: [itemActionRow, playerActionRow],
-                }
-            );
+      // Fetch players from the database
+      const players = await Player.findAll();
+      const playerOptions = players.map((player) =>
+        new StringSelectMenuOptionBuilder()
+          .setLabel(player.name)
+          .setValue(player.player_id.toString())
+      );
 
-            const collector = reply.createMessageComponentCollector({
-                ComponentType: ComponentType.StringSelect,
-                filter: (i) => i.user.id === interaction.user.id && i.customId === interaction.id,
-                time: 60_000,
-            });
+      const initialMenu = new StringSelectMenuBuilder()
+        .setCustomId('initialSelect')
+        .setPlaceholder('Select an item')
+        .addOptions(itemOptions);
 
-            collector.on('collect', (interaction) => {
-                console.log(interaction.values);
-            })
+      const initialRow = new ActionRowBuilder().addComponents(initialMenu);
+
+      await interaction.reply({
+        content: 'Please select an item:',
+        components: [initialRow],
+      });
+
+      const initialCollector = interaction.channel.createMessageComponentCollector({
+        componentType: ComponentType.SELECT_MENU,
+        time: 60000,
+      });
+
+      initialCollector.on('collect', async (initialInteraction) => {
+        if (initialInteraction.customId === 'initialSelect') {
+          const selectedItemId = initialInteraction.values[0];
+
+          const playerMenu = new StringSelectMenuBuilder()
+            .setCustomId('playerSelect')
+            .setPlaceholder('Select a player')
+            .addOptions(playerOptions);
+
+          const playerRow = new ActionRowBuilder().addComponents(playerMenu);
+
+          await initialInteraction.update({
+            content: 'Please select a player:',
+            components: [playerRow],
+          });
+
+          const playerCollector = interaction.channel.createMessageComponentCollector({
+            componentType: ComponentType.SELECT_MENU,
+            time: 60000,
+          });
+
+          playerCollector.on('collect', async (playerInteraction) => {
+            if (playerInteraction.customId === 'playerSelect') {
+              const selectedPlayerId = playerInteraction.values[0];
+
+              // Retrieve the selected item and player information from the IDs
+              const selectedItem = items.find((item) => item.item_id.toString() === selectedItemId);
+              const selectedPlayer = players.find((player) => player.player_id.toString() === selectedPlayerId);
+
+              await playerInteraction.update({
+                content: `Item '${selectedItem.item_name}' has been sent to player '${selectedPlayer.name}'.`,
+                components: [], // Remove components
+              });
+
+              // Log collected info
+              console.log(`Selected item: ${selectedItem.item_name}`);
+              console.log(`Sent to player: ${selectedPlayer.name}`);
+              // Handle the selected player or store the data as needed in the database
+            }
+          });
+
+          playerCollector.on('end', () => {
+            console.log('Player collector ended');
+          });
+        }
+      });
+
+      initialCollector.on('end', () => {
+        console.log('Initial collector ended');
+      });
+    } catch (error) {
+      console.error('Error fetching data from the database:', error);
+      // Handle error, send a message, or perform error logging as needed
     }
+  },
 };
