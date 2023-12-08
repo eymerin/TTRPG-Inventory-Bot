7a618152155ce0a1dff9e33ca224161a51e3820c
Dana <68112012+SikoticVinyl@users.noreply.github.com>
2023-12-07 17:56:26 -0700

    login works, able to login need to handle routes once logged in still.

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Player } = require('../../models');
+const bcrypt = require('bcrypt');
 
 router.post('/', async (req, res) => {
   try {
@@ -18,32 +19,37 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const playerData = await Player.findOne({ where: { username: req.body.player } });
+    console.log('Request Body:', req.body);
+
+    const playerData = await Player.findOne({ where: { username: req.body.username } });
+    console.log('Player Data:', playerData);
 
     if (!playerData) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+      console.log('User not found');
+      res.status(400).json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
-    const validPassword = await playerData.checkPassword(req.body.password);
+    // Compare the provided password (req.body.password) with the hashed password from the database
+    const validPassword = await bcrypt.compare(req.body.password, playerData.password);
+
+    console.log('Valid Password:', validPassword);
 
     if (!validPassword) {
-      res
-        .status(400)
-        .json({ message: 'Incorrect username or password, please try again' });
+      console.log('Invalid password');
+      res.status(400).json({ message: 'Incorrect username or password, please try again' });
       return;
     }
 
     req.session.save(() => {
       req.session.player_id = playerData.player_id;
       req.session.logged_in = true;
-      
+      console.log('User logged in:', playerData.username);
       res.json({ player: playerData, message: 'You are now logged in!' });
     });
 
   } catch (err) {
+    console.error('Error:', err);
     res.status(400).json(err);
   }
 });
--- public/js/login.js
+++ public/js/login.js
@@ -1,23 +1,37 @@
 const loginFormHandler = async (event) => {
     event.preventDefault();
-
+  
     const username = document.querySelector('#username').value.trim();
     const password = document.querySelector('#password').value.trim();
-
+  
     if (username && password) {
-        const response = await fetch('./api/user-routes', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
+      try {
+        console.log('Sending login request...');
+        const response = await fetch('/api/users/login', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ username, password }),
         });
-
+  
+        console.log('Response status:', response.status);
+  
         if (response.ok) {
-        document.location.replace('/');
+          // Handle successful login response
+          const responseData = await response.json();
+          console.log('Login successful:', responseData); // Log the response data
+          document.location.replace('/'); // Redirect to the home page or desired location
         } else {
-        alert('Failed to log in');
+          // Handle failed login response
+          alert('Failed to log in');
         }
+      } catch (error) {
+        console.error('Error:', error);
+        // Handle network errors or other exceptions
+        alert('Error occurred while logging in');
+      }
     }
-};
-
-document
-    .querySelector('.login')
-    .addEventListener('submit', loginFormHandler);
+  };
+  
+  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
